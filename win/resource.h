837ca5cf8eadb3848b982df80ed4ed61b0d6a21f
Pavel Karneliuk <pavel.karneliuk@gmail.com>
2013-11-08 11:39:59 +0000

    remove license.h and license.cpp

--- win/resource.h
+++ win/resource.h
@@ -4,15 +4,11 @@
 //
 #define IDD_CONFIG                      106
 #define IDC_NOSHADERS                   1000
-#define IDC_ACTIVATE                    1001
-#define IDC_DEVICE                      1002
-#define IDC_OK                          1003
-#define IDC_CANCEL                      1004
-#define IDC_SUBMIT                      1005
-#define IDC_REGISTER                    1006
-#define IDC_DEVICETEXT                  1007
-#define IDC_VFLIP                       1008
-#define IDC_HFLIP                       1009
-#define IDC_AKEY                        1011
-
+#define IDC_DEVICE                      1001
+#define IDC_OK                          1002
+#define IDC_CANCEL                      1003
+#define IDC_REGISTER                    1004
+#define IDC_DEVICETEXT                  1005
+#define IDC_VFLIP                       1006
+#define IDC_HFLIP                       1007
 
