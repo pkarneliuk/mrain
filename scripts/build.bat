fd4713b463e5bf2cfee88d93eb93ef3d5b03c4a1
Pavel Karneliuk <pavel.karneliuk@gmail.com>
2017-08-15 16:56:50 +0300

    add minimal CMake scripts

---
+++ scripts/build.bat
@@ -0,0 +1,9 @@
+SET build=..\..\build-win
+
+RMDIR /S /Q %build% & :: remove old build directory
+MKDIR %build%       & :: create new build directory
+CD %build%
+
+cmake ../mrain -G "Visual Studio 15 2017 Win64"          & :: generate project in build dir
+cmake --build . --target "ALL_BUILD" --config "Release"
+cpack -G WIX .
